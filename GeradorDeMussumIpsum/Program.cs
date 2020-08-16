187c82dbda277a0b9e7ed7b99a28b3c8542c2f6a
Anna Souza <contato.annasouza@outlook.com>
2020-08-16 03:38:36 -0300

    Primeiro Commit

---
+++ GeradorDeMussumIpsum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GeradorDeMussumIpsum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long quantidadePalavras;
+            string frase = "";
+            Random numeroAleatorio = new Random();
+            string[] palavras = { "Mussum Ipsum", "cacilds", "euzis", "litrus", "mê faiz", "elementum", "tendi nada", "vitae", "quem manda na minha terra", "consequat" };
+            Console.WriteLine("--------Gerador de Mussum Ipsum--------");
+            Console.WriteLine("Digite a quantidade de Palavras desejadas:");
+            quantidadePalavras = Convert.ToInt64(Console.ReadLine());
+            for (int i = 0; i <= quantidadePalavras; i++)
+            {
+                frase = frase + " " + palavras[numeroAleatorio.Next(0, 9)];
+            }
+            Console.WriteLine(frase);
+
+            Console.ReadKey();
+        }
+    }
+}
